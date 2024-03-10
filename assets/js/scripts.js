35e0383d345f4bb046788150139f894192ecb9cb
rakibthpi <rakibhosen2244@gmail.com>
2024-03-10 10:32:58 +0600

    this is first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	// Mobile Menu
+	// $('.logo a i').click(function(){
+	// 	$('.menu ul').slideToggle(1000);
+
+	// 	return false
+	// });
+
+	// Banner Carousel
+	// $('.banner_carousel').owlCarousel({
+	// 	items:1,
+	// 	loop:true,
+	// 	nav:true,
+	// 	dots:true,
+	// 	autoplay:true,
+	// });
+
+
+
+
+
+});
+
+	
+$(function () {
+	$('.toggle-menu').click(function(){
+	   $('.menu').toggleClass('display');
+	   
+	});
+	
+   });
+   
+	 
